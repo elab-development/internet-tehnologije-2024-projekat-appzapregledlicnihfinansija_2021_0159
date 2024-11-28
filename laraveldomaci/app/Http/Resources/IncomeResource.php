3a1580456c4725a4facc402e9c92e7648b2e6817
Aleksandra-Perovic2 <bekabeka123@gmail.com>
2024-11-29 00:19:15 +0100

    kreirani resource-i za sve modele

---
+++ laraveldomaci/app/Http/Resources/IncomeResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class IncomeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'description' => $this->description,
+            'user_id' => $this->user_id,
+            'source' => $this->source,
+            'currency' => $this->currency,
+            'date' => $this->date->format('Y-m-d'),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
