ecd7e266b67da63e7041640ba82ca45e1cc7b8aa
Aleksandra-Perovic2 <bekabeka123@gmail.com>
2024-11-28 23:57:01 +0100

    uneti testni podaci u bazu podataka

--- laraveldomaci/database/seeders/DatabaseSeeder.php
+++ laraveldomaci/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\ExpenseCategory;
+use App\Models\Expense;
+use App\Models\Income;
+use App\Models\Goal;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +24,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+         // Kreiranje korisnika
+         User::factory(20)->create(); // Generiše 20 korisnika
+
+         // Kreiranje kategorija troškova
+         ExpenseCategory::factory(50)->create(); // Generiše 50 kategorija troškova
+ 
+         // Kreiranje troškova
+         Expense::factory(100)->create(); // Generiše 100 troškova
+ 
+         // Kreiranje prihoda
+         Income::factory(100)->create(); // Generiše 100 prihoda
+ 
+         // Kreiranje ciljeva
+         Goal::factory(30)->create(); // Generiše 30 ciljeva
     }
 }
