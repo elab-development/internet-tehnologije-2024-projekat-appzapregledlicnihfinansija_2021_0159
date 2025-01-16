ec7cc40d7a7ba494eb98a255a098a7407d551e43
Aleksandra-Perovic2 <bekabeka123@gmail.com>
2025-01-16 08:22:44 +0100

    Definisan tekst mejla

---
+++ laraveldomaci/resources/views/emails/goalNotification.blade.php
@@ -0,0 +1,5 @@
+<h1>Napredak vašeg cilja: {{ $data['goalTitle'] }}</h1>
+<p>Čestitamo! Postigli ste {{ $data['percentage'] }}% vašeg cilja.</p>
+<p>Ciljani iznos: {{ $data['targetAmount'] }}</p>
+<p>Trenutno stanje: {{ $data['currentAmount'] }}</p>
+<p>Preostali iznos za ostvarenje cilja: {{ $data['remainingAmount'] }}</p>
